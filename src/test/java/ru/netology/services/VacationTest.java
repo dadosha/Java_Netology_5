608863734e9935963e44f856b41b4313ca77f522
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-03-02 20:08:47 +0300

    Return CsvSource

--- src/test/java/ru/netology/services/VacationTest.java
+++ src/test/java/ru/netology/services/VacationTest.java
@@ -3,6 +3,7 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationTest {
 
